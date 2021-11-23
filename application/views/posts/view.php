ff0205e79343fd54d83947b75b6dd65d18775173
arishavelle18 <arishavelle18@yahoo.com>
2021-11-24 02:02:13 +0800

    Di pa ayos yung D sa Crud sa Posting

--- application/views/posts/view.php
+++ application/views/posts/view.php
@@ -1,6 +1,8 @@
 <a href="<?php echo base_url();?>pages/view">
     <i class="fas fa-arrow-circle-left btn btn-secondary btn-lg px-3 py-1"></i>
 </a>
+<!--  -->
+
 <h1><?=$title;?></h1>
 <small class="post-date">Posted on <?php echo $post["created_at"];?></small>
 <div class="post-body">
