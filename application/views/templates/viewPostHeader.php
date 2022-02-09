5c7141aba3b59fa653d29e240d94091ac9e0bbe9
arishavelle18 <arishavelle18@yahoo.com>
2022-02-09 15:57:51 +0800

    Fixing search query_db

--- application/views/templates/viewPostHeader.php
+++ application/views/templates/viewPostHeader.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Microblogging Site</title>
+    <title>Thinklik</title>
     <script src="//cdn.ckeditor.com/4.16.2/standard/ckeditor.js"></script>
     <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
     <link rel="stylesheet" href="<?php echo base_url();?>/assets/css/style.css">
