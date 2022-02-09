20275ae556986065c6c5747b373c80cdc24d3b13
Ma. Diana Fama <ma.dianafama@gmail.com>
2022-02-09 18:17:49 +0800

    create and edit post improve

--- application/views/posts/create.php
+++ application/views/posts/create.php
@@ -1,38 +1,37 @@
 <div onclick="checkMousePointer()">
-<br>
-<br>
-<br>
-<br>
-<div style="width: 100vw;
-    height: 100vh;" class="container"> 
-<?php echo validation_errors();?>
-<?php echo form_open_multipart("posts/create");?>
-  <div class="form-group">
-    <label for="title">Title : </label>
-    <input type="text" class="form-control" name="title">
-  </div>
-  <div class="form-group">
-    <label for="body">Body :</label>
-    <textarea  class="form-control" id="editor1" name="body"></textarea>
-  </div>
-  <div class="form-group">
-    <label for="">Categories:</label>
-    <select name="category_id" class="form-control ">
-    <?php foreach($categories as $category):?>
-      <option value="<?php echo $category['category_id'];?>">
-        <?php echo $category['name'];?>
-      </option>
-    <?php endforeach;?>
-    </select>
-  </div>
-  <div class="form-group mt-3">
-    <label for="file">Upload Image</label>
-    <div class="mt-3 mb-3">
-      <input type="file" name="userfile" size="20"> 
+  <div style="width: 100vw; height: 100vh;" class="container"> 
+    <div class="createpost">
+      <?php echo form_open_multipart("posts/create");?>
+        <div class="form-group">
+          <label for="title">Title : </label>
+          <input type="text" class="form-control" name="title">
+        </div>
+        <div class="form-group">
+          <label for="body">Body :</label>
+          <textarea  class="form-control" id="editor1" name="body"></textarea>
+        </div>
+        <div class="form-group">
+          <label for="">Categories:</label>
+          <select name="category_id" class="form-control ">
+          <?php foreach($categories as $category):?>
+            <option value="<?php echo $category['category_id'];?>">
+              <?php echo $category['name'];?>
+            </option>
+          <?php endforeach;?>
+          </select>
+        </div>
+        <div class="form-group mt-3">
+          <label for="file" class="uploadlabel">Upload Image</label>
+          <div class="mt-3 mb-3">
+            <input class="uploadpic" type="file" name="userfile" size="20"> 
+          </div>
+        </div>
+        <button type="submit">Post</button>
+      </form>
+      <a href="<?php echo base_url();?>pages/view"><button type="submit" class="cancelbutton">Cancel</button></a>
+      <h3><?php echo validation_errors();?></h3>
     </div>
   </div>
-  <button type="submit" class="btn btn-success">Post</button>
-</form>
 </div>
 
 
